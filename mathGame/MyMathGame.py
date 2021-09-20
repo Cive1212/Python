e11aa6e82ad979d5b4c3a4af217b7af84f6a1bb2
Cive1212 <61161903+Cive1212@users.noreply.github.com>
2021-09-20 18:38:37 +0200

    abcd

---
+++ mathGame/MyMathGame.py
@@ -0,0 +1,9 @@
+a = input("9 * 9=? ")
+prawidlowa = 81
+if a.isdigit():
+    if float(a) == prawidlowa:
+        print("poprawna odpowiedź!")
+    else:
+        print("błąd! Poprawna odpowiedź to " + str(prawidlowa))
+else:
+    print("błąd podana odpowiedź nie jest liczbą!")
