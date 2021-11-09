a3365f7061d5ed53ac4bae099dfa38ba046a8848
Cive1212 <61161903+Cive1212@users.noreply.github.com>
2021-11-09 20:23:51 +0100

    change quiz logic(not finished)

--- mathGame/MyMathGame.py
+++ mathGame/MyMathGame.py
@@ -25,16 +25,41 @@ engList =   [
                 ["jak jest 'widelec' po angielsku ", "fork"],
                 ["jak jest 'kość' po angielsku ", "bone" ]
             ]
+plList =     [
+                ["jak jest 'pies' po angielsku? ", "dog"],
+                ["jak jest 'żaba' po angielsku ", "frog"],
+                ["jak są 'drzwi' po angielsku ", "door"],
+                ["jak jest 'młotek' po angielsku ", "hammer"],
+                ["jak jest 'drzewo' po angielsku ", "tree"], 
+                ["jak jest 'ptak' po angielsku ", "bird"],
+                ["jak jest 'kubek' po angielsku ", "cup"],
+                ["jak jest 'widelec' po angielsku ", "fork"],
+                ["jak jest 'kość' po angielsku ", "bone" ]
+            ]
+healthList =   [
+                ["jak jest 'pies' po angielsku? ", "dog"],
+                ["jak jest 'żaba' po angielsku ", "frog"],
+                ["jak są 'drzwi' po angielsku ", "door"],
+                ["jak jest 'młotek' po angielsku ", "hammer"],
+                ["jak jest 'drzewo' po angielsku ", "tree"], 
+                ["jak jest 'ptak' po angielsku ", "bird"],
+                ["jak jest 'kubek' po angielsku ", "cup"],
+                ["jak jest 'widelec' po angielsku ", "fork"],
+                ["jak jest 'kość' po angielsku ", "bone" ]
+            ]
  
 myList["math"] = mathList
 myList["eng"] = engList
-indexes = [list(range(0,len(myList["math"]))), list(range(0,len(myList["eng"])))]
+myList["polski"] = plList
+myList["zywienie"] = healthList
+#indexes = [list(range(0,len(myList["math"]))), list(range(0,len(myList["eng"])))]
 points = 0
-pointsM = -1
-pointsA = -1
-pointsP = -1
-pointsH = -1
- 
+
+def generateList(x):
+    indexes = []
+    indexes.append(list(range(0,len(myList[x]))))
+    return indexes
+
 def verifyNumber(x):
     while True:
         answer = input(x)
@@ -43,95 +68,60 @@ def verifyNumber(x):
             return int(answer)
         else:
             print("błąd podana odpowiedź nie jest liczbą, podaj liczbę!")
- 
-def mathQuiz():
-    pointsM = 0
- 
+
+def quiz(x):
+    y = 0
+
     for i in range(4):
-        questionNumber = random.choice(indexes[0])
-        indexes[0].pop(indexes[0].index(questionNumber))
- 
-        if verifyNumber(myList["math"][questionNumber][0]) == myList["math"][questionNumber][1]:
-            print("poprawna odpowiedź!")
-            pointsM += 1
+        indexList = generateList(x)
+        questionNumber = random.choice(indexList)
+        indexList.pop(indexList.index(questionNumber))
+
+        if x == "math":
+
+            if verifyNumber(myList["math"][questionNumber][0]) == myList["math"][questionNumber][1]:
+                print("poprawna odpowiedź!")
+                y += 1
+            else:
+                print("błąd! Poprawna odpowiedź to " + str(myList["math"][questionNumber][1]))
+
         else:
-            print("błąd! Poprawna odpowiedź to " + str(myList["math"][questionNumber][1]))
- 
-    print("uzyskałeś " + str(pointsM) + " punktów")
-    return pointsM
- 
-def engQuiz():
-    pointsA = 0
- 
-    for i in range(4):
-        questionNumber = random.choice(indexes[1])
-        indexes[1].pop(indexes[1].index(questionNumber))
-        answer = input(myList["eng"][questionNumber][0])
- 
-        if answer.lower() == myList["eng"][questionNumber][1]:
-            print("poprawna odpowiedź!")
-            pointsA += 1
-        else:
-            print("błąd! Poprawna odpowiedź to " + str(myList["eng"][questionNumber][1]))
-            
-    print("uzyskałeś " + str(pointsA) + " punktów")
-    return pointsA
- 
-def plQuiz():
-    pointsP = 0
-    print("jestem testem z polskiego")
-    return pointsP
- 
-def healthQuiz():
-    pointsH =0
-    print("jestem testem z żywienia")
-    return pointsH
+            answer = input(myList[x][questionNumber][0])
+
+            if answer.lower() == myList[x][questionNumber][1]:
+                print("poprawna odpowiedź!")
+                y += 1
+            else:
+                print("błąd! Poprawna odpowiedź to " + str(myList["x"][questionNumber][1]))
+
+    print("uzyskałeś " + str(y) + " punktów")
+    return y 
  
 print("Witaj Użytkowniku!")
-print("wybierz quiz z listy wpisując jego numer")
- 
+
 for i in range(4):
- 
+
     while True:
-        answer = verifyNumber("1. matematyka 2. angielski 3. polski 4. żywienie ")
- 
-        if answer == 1:
- 
-            if pointsM >= 0:
-                print ("ten quiz został już wykonany")
+        restart = False
+
+        for i in myList:
+            answer = input("podaj nazwę quizu który chcesz rozwiązać ")
+
+            if answer.lower() == i:
+                print (list(myList).index(i))
+                points = points + quiz(i)
+                itemToPop = i
+                restart = True
             else:
-                pointsM = mathQuiz()
-                myList["math"].pop(answer - 1)
-                break
- 
-        elif answer == 2:
- 
-            if pointsA >= 0:
-                print ("ten quiz został już wykonany")
-            else:
-                pointsA = engQuiz()
-                myList["eng"].pop(answer - 1)
-                break
- 
-        elif answer == 3:
- 
-            if pointsP >= 0:
-                print ("ten quiz został już wykonany")
-            else:
-                pointsP = plQuiz()
-                break
-        elif answer == 4:
- 
-            if pointsH >= 0:
-                print ("ten quiz został już wykonany")
-            else:
-                pointsH = healthQuiz()
-                break
- 
-        else:
-            print("podaj poprawny numer quizu")
- 
-points = int(pointsM) + int(pointsA) + int(pointsP) + int(pointsH)
+                print ("podaj poprawną nazwę quizu!")
+
+        if itemToPop != 0:
+            myList.pop(itemToPop)
+            itemToPop = 0
+
+        if restart == True:
+            break
+    
 print ("koniec quizów")
 print ("uzyskałeś " +str(points) + "/8 punktow")
  
