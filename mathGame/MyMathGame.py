08f27db36b37ba7239e33ad88cc0ec255f444175
Cive1212 <61161903+Cive1212@users.noreply.github.com>
2021-11-10 20:51:33 +0100

    format tables and remove space after function names

--- mathGame/MyMathGame.py
+++ mathGame/MyMathGame.py
@@ -3,45 +3,48 @@ import random
 myList = {}
  
 mathList = [
-                ["9 * 9=? ", 81],
-                ["15 + 10=? ", 25],
-                ["50 - 17=? ", 33],
-                ["10 * 9= ?", 90],
-                ["8 + 6=? ", 14],
-                ["5 * 5=? ", 25],
-                ["4 * 4=? ", 16],
-                ["4 * 9= ? ", 36],
-                ["6 * 6=? ", 36],
-                ["10:2=? ", 5]
-            ]
+    ["9 * 9=? ", 81],
+    ["15 + 10=? ", 25],
+    ["50 - 17=? ", 33],
+    ["10 * 9= ?", 90],
+    ["8 + 6=? ", 14],
+    ["5 * 5=? ", 25],
+    ["4 * 4=? ", 16],
+    ["4 * 9= ? ", 36],
+    ["6 * 6=? ", 36],
+    ["10:2=? ", 5]
+]
+
 engList = [
-                ["jak jest 'pies' po angielsku? ", "dog"],
-                ["jak jest 'żaba' po angielsku ", "frog"],
-                ["jak są 'drzwi' po angielsku ", "door"],
-                ["jak jest 'młotek' po angielsku ", "hammer"],
-                ["jak jest 'drzewo' po angielsku ", "tree"], 
-                ["jak jest 'ptak' po angielsku ", "bird"],
-                ["jak jest 'kubek' po angielsku ", "cup"],
-                ["jak jest 'widelec' po angielsku ", "fork"],
-                ["jak jest 'kość' po angielsku ", "bone" ]
-            ]
+    ["jak jest 'pies' po angielsku? ", "dog"],
+    ["jak jest 'żaba' po angielsku ", "frog"],
+    ["jak są 'drzwi' po angielsku ", "door"],
+    ["jak jest 'młotek' po angielsku ", "hammer"],
+    ["jak jest 'drzewo' po angielsku ", "tree"], 
+    ["jak jest 'ptak' po angielsku ", "bird"],
+    ["jak jest 'kubek' po angielsku ", "cup"],
+    ["jak jest 'widelec' po angielsku ", "fork"],
+    ["jak jest 'kość' po angielsku ", "bone" ]
+]
+
 plList = [
-                ["czym jest synonim? a. wyrazem podobnym b. wyrazem o przeciwnym znaczeniu c. dlugim wyrazem ", "a"],
-                ["w którym roku odbył się chrzest Polski? a. 1042 b. 966 c.894 d. 1410 ", "b"],
-                ["w którym roku odbyła się bitwa pod Grunwaldem? a. 1240 b. 1410 c.1330 d. 1505 ", "b"],
-                ["'pies' to: a. rzeczownik b. czasownik c. przymiotnik ", "a"],
-                ["kiedy obchodzimy świeto niepodległości? a. 11.11 b 12. 11 c 07.13 d 09.15 ", "a"], 
-                ["jakie zwierzęta są symbolem białowieskiego parku narodowego? a. żubr b. bizon c. dzięciął d. krokodyl ", "a"],
-            ]
+    ["czym jest synonim? a. wyrazem podobnym b. wyrazem o przeciwnym znaczeniu c. dlugim wyrazem ", "a"],
+    ["w którym roku odbył się chrzest Polski? a. 1042 b. 966 c.894 d. 1410 ", "b"],
+    ["w którym roku odbyła się bitwa pod Grunwaldem? a. 1240 b. 1410 c.1330 d. 1505 ", "b"],
+    ["'pies' to: a. rzeczownik b. czasownik c. przymiotnik ", "a"],
+    ["kiedy obchodzimy świeto niepodległości? a. 11.11 b 12. 11 c 07.13 d 09.15 ", "a"], 
+    ["jakie zwierzęta są symbolem białowieskiego parku narodowego? a. żubr b. bizon c. dzięciął d. krokodyl ", "a"],
+]
+
 healthList = [
-                ["ktore owoce maja duzo witaminy c? a. jabłko b. kiwi c.cytryny d.arbuzy ", "bc"],
-                ["które produkty maja wysoki indeks glikemiczny? a. ryż b. jabłko c.frytki d. płatki owsiane ", "acd"],
-                ["na czym można smażyć? a.olej b.masło c.ocet d.bulion ", "ab"],
-                ["ktore ryby sa slodkowodne? a. karp b. ukleja c. węgorz d. lin ", "abd"],
-                ["które tłuszcze mają wysoki punkt dymienia? a. masło b. masło klarowane c. olej lniany d. łój  ", "bd"], 
-                ["które z podanych to akcesoria kuchenne? a. salamandra b. wilk c. tygrys d. kot ", "ab"],
-                ["które produkty mają dobry cholesterol? a. frytki b. oliwa z oliwek c. łosoś d. wino ", "bcd"],
-            ]
+    ["ktore owoce maja duzo witaminy c? a. jabłko b. kiwi c.cytryny d.arbuzy ", "bc"],
+    ["które produkty maja wysoki indeks glikemiczny? a. ryż b. jabłko c.frytki d. płatki owsiane ", "acd"],
+    ["na czym można smażyć? a.olej b.masło c.ocet d.bulion ", "ab"],
+    ["ktore ryby sa slodkowodne? a. karp b. ukleja c. węgorz d. lin ", "abd"],
+    ["które tłuszcze mają wysoki punkt dymienia? a. masło b. masło klarowane c. olej lniany d. łój  ", "bd"], 
+    ["które z podanych to akcesoria kuchenne? a. salamandra b. wilk c. tygrys d. kot ", "ab"],
+    ["które produkty mają dobry cholesterol? a. frytki b. oliwa z oliwek c. łosoś d. wino ", "bcd"],
+]
  
 myList["matematyka"] = mathList
 myList["angielski"] = engList
@@ -91,7 +94,7 @@ for i in range(4):
 
     while True:
         restart = False
-        print (list(myList))
+        print(list(myList))
         answer = input("podaj nazwę quizu który chcesz rozwiązać i upewnij się, że jest poprawna! ")
 
         for i in myList:
@@ -109,12 +112,12 @@ for i in range(4):
         if restart == True:
             break
     
-print ("koniec quizów")
-print ("uzyskałeś " +str(points) + "/16 punktow")
+print("koniec quizów")
+print("uzyskałeś " +str(points) + "/16 punktow")
  
 if points >=10:
-    print ("zdałeś egzamin!")
+    print("zdałeś egzamin!")
 else:
-    print ("niestety nie udało ci się zdać, wróć w sierpniu")
+    print("niestety nie udało ci się zdać, wróć w sierpniu")
  
 input("press enter to exit")
